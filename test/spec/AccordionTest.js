6d6fa6c468068f1884cb537b80e1370a58b40b77
Eddie McWhirter <emcwhirter@usgs.gov>
2014-07-08 14:16:20 -0600

    Add initial accordion tests

---
+++ test/spec/AccordionTest.js
@@ -0,0 +1,66 @@
+/* global define, describe, it, beforeEach */
+define([
+	'chai',
+
+	'Accordion'
+], function (
+	chai,
+
+	Accordion
+) {
+	'use strict';
+	var expect = chai.expect;
+
+	var emptyAccordion = new Accordion({
+		el: document.querySelector('.page-content'),
+		accordions: [
+			{} // Empty accordion - use all of the defaults
+		]
+	});
+
+	describe('Accordion test suite', function () {
+
+		describe('_initialize()', function () {
+			it('Is defined', function () {
+				expect(typeof Accordion).to.equal('function');
+			});
+
+			it('Can be instantiated.', function () {
+				expect(emptyAccordion).to.be.an.instanceof(Accordion);
+			});
+
+			it('Sets options on itself.', function () {
+				expect(emptyAccordion).to.be.an.instanceof(Object);
+			});
+
+			it('Can be required.', function () {
+				/* jshint -W030 */
+				expect(Accordion).to.not.be.null;
+				/* jshint +W030 */
+			});
+
+			it('Has all expected methods.', function () {
+				expect(emptyAccordion).to.respondTo('_initialize');
+				expect(emptyAccordion).to.respondTo('addAccordion');
+			});
+
+			it('Has all expected properties.', function () {
+				expect(emptyAccordion).to.have.property('_options');
+				expect(emptyAccordion).to.have.property('_el');
+			});
+
+			it('Has proper default attributes.', function () {
+				/* jshint -W030 */
+				expect(emptyAccordion.el).to.not.be.null;
+				expect(emptyAccordion.accordions).to.not.be.null;
+				expect(emptyAccordion.accordion).to.not.be.null;
+				expect(emptyAccordion.title).to.not.be.null;
+				expect(emptyAccordion.content).to.not.be.null;
+				/* jshint +W030 */
+			});
+
+		});
+
+	});
+
+});
