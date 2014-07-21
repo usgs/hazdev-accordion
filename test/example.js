cf30bfdc2721d8e2c67aa68b4d3d269b931fafae
ehunter-usgs <ehunter@usgs.gov>
2014-07-21 16:54:00 -0600

    Update styles. Update examples to be more informative.

--- test/example.js
+++ test/example.js
@@ -9,6 +9,18 @@ require([
 ) {
 	'use strict';
 
+	new Accordion({
+		el:document.querySelector('.accordion-set-1')
+	});
+
+	new Accordion({
+		el:document.querySelector('.accordion-set-2')
+	});
+
+	new Accordion({
+		el:document.querySelector('.accordion-set-3')
+	});
+
 	var accordion = new Accordion({
 		el: document.querySelector('.test-accordions'),
 		accordions: [
@@ -16,31 +28,33 @@ require([
 				toggleText: 'Section Header (Dynamically Created)',
 				toggleElement: 'h3',
 				contentText:
-					'This accordion element is not part of the original DOM, but is ' +
+					'<p>This accordion element is not part of the original DOM, but is ' +
 					'injected by Javascript at load time. Just an example of what can ' +
-					'be done. Using the <em>accordion.addAccordion</em> method will' +
-					'create a "standard" accordion with a header/banner at the top' +
-					'being used for the clickable toggle.'
+					'be done. Using the <em>accordion.addAccordion</em> method will ' +
+					'create a "standard" accordion with a header/banner at the top ' +
+					'being used for the clickable toggle.</p>' +
+					'<p>If this accordion were created with <em>accordion-closed</em> ' +
+					'defined as one of the classes. That class would ensure that the ' +
+					'accordion was collpased by default.</p>'
 			},
-			{}, // Empty accordion - use all of the defaults
 			{
-				toggleText: 'Another dynamic section',
+				toggleText: 'Section Header 2 (Dynamically Created)',
 				toggleElement: 'span',
 				contentText:
-					'My awesome contents for this great accordion',
-				classes: 'my-accordion'
+					'<p>My awesome contents for this great accordion</p>',
+				classes: 'accordion-section example-style'
 			}
 		]
 	});
 
 	// build from content/title passed in
 	accordion.addAccordion({
-		toggleText: 'Section Header (Dynamically Created part 2)',
+		toggleText: 'Section Header 3 (Dynamically Created)',
 		contentText:
-		'This accordion element is also not part of the original DOM, but is ' +
+		'<p>This accordion element is also not part of the original DOM, but is ' +
 		'injected by Javascript at load time. This accordion is also not part ' +
 		'of the initial accordion creation, it was added later with a direct ' +
-		'call to <em>addAccordion()</em>. All your base are belong to us.'
+		'call to <em>addAccordion()</em>. All your base are belong to us.</p>'
 	});
 
 });
