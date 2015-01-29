cce2266bf112459186c64a7b553fa30b0bb3a1ca
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 08:27:49 -0700

    set up watch file for grunt

--- gruntconfig/watch.js
+++ gruntconfig/watch.js
@@ -5,15 +5,43 @@ var config = require('./config');
 var watch = {
   scripts: {
     files: [config.src + '/**/*.js'],
-    tasks: ['jshint:scripts', 'browserify', 'mocha_phantomjs']
+    tasks: [
+      'jshint:scripts',
+      'browserify',
+      'mocha_phantomjs'
+    ]
   },
   tests: {
-    files: [config.test + '/*.html', config.test + '/**/*.js'],
-    tasks: [ 'jshint:tests', 'browserify', 'mocha_phantomjs']
+    files: [
+      config.test + '/*.html',
+      config.test + '/**/*.js'
+    ],
+    tasks: [
+      'jshint:tests',
+      'browserify',
+      'mocha_phantomjs'
+    ]
+  },
+  scss: {
+    files: [
+      config.src + '/**/*.scss',
+      config.src + '/**/*.scss'
+    ],
+    tasks: ['compass:dev', 'compass:test']
   },
   gruntfile: {
-    files: ['Gruntfile.js'],
+    files: [
+      'Gruntfile.js',
+      'gruntconfig/**/*.js'
+    ],
     tasks: ['jshint:gruntfile']
+  },
+  example: {
+    files: [
+      config.example + '/*.html',
+      config.example + '/**/*.js'
+    ],
+    tasks: ['build']
   }
 };
 
