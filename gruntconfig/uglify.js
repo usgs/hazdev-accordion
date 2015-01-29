480aa3fa99936e406a27c52d8af6e0275c851238
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 08:23:11 -0700

    uglifies files that are placed in dist

---
+++ gruntconfig/uglify.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var config = require('./config');
+
+var uglify = {
+  build: {
+    files: {
+    }
+  }
+};
+
+uglify.build.files[config.dist + '/index.js'] = config.build + '/example/index.js';
+uglify.build.files[config.dist + '/Accordion.js'] = config.build + '/src/Accordion.js';
+module.exports = uglify;
