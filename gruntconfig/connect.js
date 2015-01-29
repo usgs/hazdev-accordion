df797a43cba41e2729e9f8bb1e66341e63a790fa
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 08:14:27 -0700

    set up connect

--- gruntconfig/connect.js
+++ gruntconfig/connect.js
@@ -9,15 +9,31 @@ var connect = {
   dev: {
     options: {
       base: [
-        config.build + '/' + config.test,
-        config.build + '/' + config.src,
-        config.test,
-        config.src,
-        'node_modules'
+        config.build + '/' + config.example,
+        config.build + '/' + config.src
       ],
       port: 8000
     }
-  }
+  },
+  test: {
+    options: {
+      base: [
+        config.build + '/' + config.test,
+        config.build + '/' + config.src,
+        'node_modules'
+      ],
+      port: 8001
+    }
+  },
+  dist: {
+    options: {
+      keepalive: true,
+      base: [
+        config.dist
+      ],
+      port: 8002
+    }
+  },
 };
 
 module.exports = connect;
