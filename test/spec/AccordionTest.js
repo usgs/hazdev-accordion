0f9b0664e29fc721c7108f38198f1c7c8581ea86
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 08:59:28 -0700

    replaced tabs with spaces

--- test/spec/AccordionTest.js
+++ test/spec/AccordionTest.js
@@ -6,54 +6,54 @@ var expect = chai.expect;
 var Accordion = require('Accordion');
 
 var emptyAccordion = new Accordion({
-	el: document.querySelector('.page-content'),
-	accordions: [
-		{} // Empty accordion - use all of the defaults
-	]
+  el: document.querySelector('.page-content'),
+  accordions: [
+    {} // Empty accordion - use all of the defaults
+  ]
 });
 
 describe('Accordion test suite', function () {
 
-	describe('_initialize()', function () {
-		it('Is defined', function () {
-			expect(typeof Accordion).to.equal('function');
-		});
+  describe('_initialize()', function () {
+    it('Is defined', function () {
+      expect(typeof Accordion).to.equal('function');
+    });
 
-		it('Can be instantiated.', function () {
-			expect(emptyAccordion).to.be.an.instanceof(Accordion);
-		});
+    it('Can be instantiated.', function () {
+      expect(emptyAccordion).to.be.an.instanceof(Accordion);
+    });
 
-		it('Sets options on itself.', function () {
-			expect(emptyAccordion).to.be.an.instanceof(Object);
-		});
+    it('Sets options on itself.', function () {
+      expect(emptyAccordion).to.be.an.instanceof(Object);
+    });
 
-		it('Can be required.', function () {
-			/* jshint -W030 */
-			expect(Accordion).to.not.be.null;
-			/* jshint +W030 */
-		});
+    it('Can be required.', function () {
+      /* jshint -W030 */
+      expect(Accordion).to.not.be.null;
+      /* jshint +W030 */
+    });
 
-		it('Has all expected methods.', function () {
-			expect(emptyAccordion).to.respondTo('_initialize');
-			expect(emptyAccordion).to.respondTo('addAccordion');
-		});
+    it('Has all expected methods.', function () {
+      expect(emptyAccordion).to.respondTo('_initialize');
+      expect(emptyAccordion).to.respondTo('addAccordion');
+    });
 
-		it('Has all expected properties.', function () {
-			expect(emptyAccordion).to.have.property('_options');
-			expect(emptyAccordion).to.have.property('_el');
-		});
+    it('Has all expected properties.', function () {
+      expect(emptyAccordion).to.have.property('_options');
+      expect(emptyAccordion).to.have.property('_el');
+    });
 
-		it('Has proper default attributes.', function () {
-			/* jshint -W030 */
-			expect(emptyAccordion.el).to.not.be.null;
-			expect(emptyAccordion.accordions).to.not.be.null;
-			expect(emptyAccordion.accordion).to.not.be.null;
-			expect(emptyAccordion.title).to.not.be.null;
-			expect(emptyAccordion.content).to.not.be.null;
-			/* jshint +W030 */
-		});
+    it('Has proper default attributes.', function () {
+      /* jshint -W030 */
+      expect(emptyAccordion.el).to.not.be.null;
+      expect(emptyAccordion.accordions).to.not.be.null;
+      expect(emptyAccordion.accordion).to.not.be.null;
+      expect(emptyAccordion.title).to.not.be.null;
+      expect(emptyAccordion.content).to.not.be.null;
+      /* jshint +W030 */
+    });
 
-	});
+  });
 
 });
 
