9c28ab7ed16e0486ef5b96d9f87659c4dde6dbfc
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-29 09:08:54 -0700

    replaced tabs with spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,33 +2,33 @@
 
 module.exports = function (grunt) {
 
-	var gruntConfig = require('./gruntconfig');
+  var gruntConfig = require('./gruntconfig');
 
-	gruntConfig.tasks.forEach(grunt.loadNpmTasks);
-	grunt.initConfig(gruntConfig);
+  gruntConfig.tasks.forEach(grunt.loadNpmTasks);
+  grunt.initConfig(gruntConfig);
 
-	grunt.registerTask('build', [
-		'clean',
-		'browserify',
-		'compass',
-		'copy',
-		'jshint'
-	]);
+  grunt.registerTask('build', [
+    'clean',
+    'browserify',
+    'compass',
+    'copy',
+    'jshint'
+  ]);
 
-	grunt.registerTask('default', [
-		'build',
-		'connect:dev',
-		'connect:test',
-		'mocha_phantomjs',
-		'watch'
-	]);
+  grunt.registerTask('default', [
+    'build',
+    'connect:dev',
+    'connect:test',
+    'mocha_phantomjs',
+    'watch'
+  ]);
 
-	grunt.registerTask('dist', [
-		'build',
-		'cssmin',
-		'htmlmin',
-		'uglify',
-		'connect:dist'
-	]);
+  grunt.registerTask('dist', [
+    'build',
+    'cssmin',
+    'htmlmin',
+    'uglify',
+    'connect:dist'
+  ]);
 };
 
