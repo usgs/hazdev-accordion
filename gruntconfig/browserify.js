6644dce01a0919f75a34be3249bc76711ef3c7df
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-03 07:21:49 -0700

    removed old code

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -2,30 +2,6 @@
 
 var config = require('./config');
 
-// var browserify = {
-//   options: {
-//     browserifyOptions: {
-//       debug: true,
-//       paths: [
-//         process.cwd() + '/' + config.src
-//       ]
-//     }
-//   }
-// };
-
-// // bundles
-// [
-//   'test/index',
-//   'example/index',
-//   'src/Accordion'
-// ].forEach(function (bundle) {
-//   var targetFile = config.build + '/' + bundle + '.js';
-//   var sourceFile = bundle + '.js';
-
-//   browserify[bundle] = {files: {}};
-//   browserify[bundle].files[targetFile] = [sourceFile];
-// });
-
 var browserify = {
   options: {
     browserifyOptions: {
