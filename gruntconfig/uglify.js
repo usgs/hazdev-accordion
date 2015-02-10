ad3af51f0e1fd084a97172364f39ac6050d4a062
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-10 08:04:06 -0700

    re wrote uglify file

--- gruntconfig/uglify.js
+++ gruntconfig/uglify.js
@@ -3,15 +3,12 @@
 var config = require('./config');
 
 var uglify = {
-  build: {
-    files: {
-    }
+  options: {
+  },
+  dist: {
+    src: config.build + '/' + config.src + '/hazdev-accordion.js',
+    dest: config.dist + '/hazdev-accordion.js'
   }
 };
-
-// uglify.build.files[config.dist + '/index.js'] =
-//     config.build + '/example/index.js';
-uglify.build.files[config.dist + '/Accordion.js'] =
-    config.build + '/src/Accordion.js';
 
 module.exports = uglify;
