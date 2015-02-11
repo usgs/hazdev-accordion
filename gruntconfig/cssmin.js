d3642d51e263998e6125cffb23d15e1cebe768af
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-11 10:34:12 -0700

    updated code

--- gruntconfig/cssmin.js
+++ gruntconfig/cssmin.js
@@ -3,12 +3,16 @@
 var config = require('./config');
 
 var cssmin = {
-  build: {
-    expand: true,
-    cwd: config.build + '/src',
-    src: '**/*.css',
-    dest: config.dist
+  dist: {
+    src: config.build + '/' + config.src + '/accordion/Accordion.css',
+    dest: config.dist + '/hazdev-accordion.css'
   }
+  // build: {
+  //   expand: true,
+  //   cwd: config.build + '/src',
+  //   src: '**/*.css',
+  //   dest: config.dist
+  // }
 };
 
 module.exports = cssmin;
