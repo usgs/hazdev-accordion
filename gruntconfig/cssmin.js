be3f991514a9f74b78a16d8791c0a931d3a60d21
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-11 10:39:55 -0700

    removed commented out code

--- gruntconfig/cssmin.js
+++ gruntconfig/cssmin.js
@@ -7,12 +7,6 @@ var cssmin = {
     src: config.build + '/' + config.src + '/accordion/Accordion.css',
     dest: config.dist + '/hazdev-accordion.css'
   }
-  // build: {
-  //   expand: true,
-  //   cwd: config.build + '/src',
-  //   src: '**/*.css',
-  //   dest: config.dist
-  // }
 };
 
 module.exports = cssmin;
