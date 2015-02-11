8125eacc95a30ac8573797fed7c31aea1efd2215
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-02-11 15:40:38 -0700

    fixed path

--- gruntconfig/cssmin.js
+++ gruntconfig/cssmin.js
@@ -4,7 +4,7 @@ var config = require('./config');
 
 var cssmin = {
   dist: {
-    src: config.build + '/' + config.src + '/accordion/Accordion.css',
+    src: config.build + '/' + config.src + '/hazdev-accordion.css',
     dest: config.dist + '/hazdev-accordion.css'
   }
 };
