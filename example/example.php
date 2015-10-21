d624b8fcc900403e8d67c5ed5f2bd0c6ee23b5b7
Hal Simpson <hasimpson@usgs.gov>
2015-10-21 17:39:13 -0600

    Changed examples to use template

---
+++ example/example.php
@@ -0,0 +1,10 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Examples Index';
+}
+include '_example.inc.php';
+?>
+
+<ul>
+  <li><a href="accordion.php<?php echo $templateLink ?>">Accordion</a></li>
+</ul>
